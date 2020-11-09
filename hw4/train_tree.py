20fe617dd9041b2bc71c434080d0a447a4372bb6
brawnerq <31658792+brawnerq@users.noreply.github.com>
2020-11-09 16:23:48 -0500

    hw 4 code completed

--- hw4/train_tree.py
+++ hw4/train_tree.py
@@ -52,8 +52,8 @@ def train_tree_greedy(
             return LeafNode(x_NF, y_N)
         else:
             # TODO recursively call train_tree_greedy to build the left child
-            left_child = None # TODO fixme
+            left_child = train_tree_greedy(x_LF, y_L, depth - 1)
             # TODO recursively call train_tree_greedy to build the right child
             right_child = None # TODO fixme
-            return InternalDecisionNode(
+            return InternalDecisionNode(x_RF, y_R, depth - 1)
                 x_NF, y_N, feat_id, thresh_val, left_child, right_child)
