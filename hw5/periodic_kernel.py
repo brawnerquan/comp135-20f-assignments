f674097fea8450093259c9866c481840f48b451c
brawnerq <31658792+brawnerq@users.noreply.github.com>
2020-11-30 19:23:42 -0500

    hw5 code complete

--- hw5/periodic_kernel.py
+++ hw5/periodic_kernel.py
@@ -3,46 +3,7 @@ Defines function `calc_periodic_kernel`
 
 Examples
 --------
->>> np.set_printoptions(precision=3, suppress=1)
 
-# Part 1: Simple kernel evaluations with F=1 features
->>> x_zero_11 = np.asarray([[0.0]])
->>> x_one_11 = np.asarray([[1.0]])
-
-# Kernel of x=0.0 with itself should be 1.0
->>> k_11 = calc_periodic_kernel(x_zero_11, x_zero_11, length_scale=2.0, period=0.3)
->>> k_11.ndim
-2
->>> k_11
-array([[1.]])
-
-# Kernel of x=1.0 with itself should be 1.0
->>> calc_periodic_kernel(x_one_11, x_one_11, length_scale=2.0, period=0.3)
-array([[1.]])
-
-# Kernel of x and z=x+period should be 1.0
->>> p = 0.3
->>> calc_periodic_kernel(x_one_11, x_one_11 + p, length_scale=2.0, period=p)
-array([[1.]])
->>> calc_periodic_kernel(x_one_11, x_one_11 + 3 * p, length_scale=2.0, period=p)
-array([[1.]])
-
-# A few other tests
->>> calc_periodic_kernel(x_one_11, x_zero_11, length_scale=0.5, period=0.3)
-array([[0.223]])
->>> calc_periodic_kernel(x_one_11, x_zero_11, length_scale=2.0, period=0.95)
-array([[0.997]])
-
-# Part 2: Kernel evaluations with several examples at once (still F=1)
->>> x_train_31 = np.asarray([[0.0], [1.0], [2.0]])
->>> calc_periodic_kernel(x_train_31, x_train_31, length_scale=2.0, period=0.95)
-array([[1.   , 0.997, 0.987],
-       [0.997, 1.   , 0.997],
-       [0.987, 0.997, 1.   ]])
->>> x_test_21 = np.asarray([[-0.5], [0.5]])
->>> calc_periodic_kernel(x_test_21, x_train_31, length_scale=2.0, period=0.95)
-array([[0.883, 0.889, 0.9  ],
-       [0.883, 0.883, 0.889]])
 '''
 
 import numpy as np
@@ -62,7 +23,7 @@ def calc_periodic_kernel(x_QF, x_train_NF=None, length_scale=1.0, period=1.0):
     x_train_NF : 2D numpy array, shape (N, F) = (n_train_examples, n_features)
         Feature array for reference *training* dataset
         Each row corresponds to the feature vector on example
-        
+
     Returns
     -------
     k_QN : 2D numpy array, shape (Q, N)
@@ -75,12 +36,56 @@ def calc_periodic_kernel(x_QF, x_train_NF=None, length_scale=1.0, period=1.0):
     Q, F = x_QF.shape
     N, F2 = x_train_NF.shape
     assert F == F2
-    
+
     k_QN = np.zeros((Q, N))
     # TODO compute kernel between rows of x_QF and rows of x_train_NF
-
+    for i in range(Q):
+        for  j in range(N):
+            k_QN[i][j] = np.exp(-0.5 *(np.square(np.sin((np.pi / period) * (x_train_NF[j] - x_QF[i]))) / np.square(length_scale)))
     # Ensure the kernel matrix positive definite
     # By adding a small positive to the diagonal
     M = np.minimum(Q, N)
     k_QN[:M, :M] += 1e-08 * np.eye(M)
     return k_QN
+
+# 
+# np.set_printoptions(precision=3, suppress=1)
+#
+# # Part 1: Simple kernel evaluations with F=1 features
+# x_zero_11 = np.asarray([[0.0]])
+# x_one_11 = np.asarray([[1.0]])
+#
+# # Kernel of x=0.0 with itself should be 1.0
+# k_11 = calc_periodic_kernel(x_zero_11, x_zero_11, length_scale=2.0, period=0.3)
+# print(k_11.ndim)
+# # 2
+# print(k_11)
+# # array([[1.]])
+#
+# # Kernel of x=1.0 with itself should be 1.0
+# print(calc_periodic_kernel(x_one_11, x_one_11, length_scale=2.0, period=0.3))
+# # array([[1.]])
+#
+# # Kernel of x and z=x+period should be 1.0
+# p = 0.3
+# print(calc_periodic_kernel(x_one_11, x_one_11 + p, length_scale=2.0, period=p))
+# # array([[1.]])
+# print(calc_periodic_kernel(x_one_11, x_one_11 + 3 * p, length_scale=2.0, period=p))
+# # array([[1.]])
+#
+# # A few other tests
+# print(calc_periodic_kernel(x_one_11, x_zero_11, length_scale=0.5, period=0.3))
+# # array([[0.223]])
+# print(calc_periodic_kernel(x_one_11, x_zero_11, length_scale=2.0, period=0.95))
+# # array([[0.997]])
+#
+# # Part 2: Kernel evaluations with several examples at once (still F=1)
+# x_train_31 = np.asarray([[0.0], [1.0], [2.0]])
+# print(calc_periodic_kernel(x_train_31, x_train_31, length_scale=2.0, period=0.95))
+# # array([[1.   , 0.997, 0.987],
+# #        [0.997, 1.   , 0.997],
+# #        [0.987, 0.997, 1.   ]])
+# x_test_21 = np.asarray([[-0.5], [0.5]])
+# print(calc_periodic_kernel(x_test_21, x_train_31, length_scale=2.0, period=0.95))
+# # array([[0.883, 0.889, 0.9  ],
+# #        [0.883, 0.883, 0.889]])
--- hw5/sqexp_kernel.py
+++ hw5/sqexp_kernel.py
@@ -3,36 +3,7 @@ Defines function `calc_sqexp_kernel`
 
 Examples
 --------
->>> np.set_printoptions(precision=3, suppress=1)
 
-# Example 1: Simple kernel evaluations with F=1 features
->>> x_zero_11 = np.asarray([[0.0]])
->>> x_one_11 = np.asarray([[1.0]])
->>> k_11 = calc_sqexp_kernel(x_zero_11, x_zero_11, length_scale=1.0)
->>> k_11.ndim
-2
->>> k_11
-array([[1.]])
->>> calc_sqexp_kernel(x_one_11, x_one_11, length_scale=1.0)
-array([[1.]])
->>> calc_sqexp_kernel(x_one_11, x_zero_11, length_scale=1.0)
-array([[0.368]])
-
-
-# Example 2: Kernel evaluations with F=2 features and several examples at once
->>> x_train_32 = np.asarray([[0.0, 0.0], [1.0, 1.0], [2.0, 2.0]])
->>> k_33 = calc_sqexp_kernel(x_train_32, x_train_32)
->>> k_33
-array([[1.   , 0.135, 0.   ],
-       [0.135, 1.   , 0.135],
-       [0.   , 0.135, 1.   ]])
-
->>> x_test_22 = np.asarray([[-1.0, -2.0], [2.2, 0.8]])
->>> k_23 = calc_sqexp_kernel(x_test_22, x_train_32)
->>> k_23
-array([[0.007, 0.   , 0.   ],
-       [0.004, 0.228, 0.228]])
->>> 
 '''
 
 import numpy as np
@@ -52,7 +23,7 @@ def calc_sqexp_kernel(x_QF, x_train_NF=None, length_scale=1.0):
     x_train_NF : 2D numpy array, shape (N, F) = (n_train_examples, n_features)
 		Feature array for reference *training* dataset
 		Each row corresponds to the feature vector on example
-		
+
     Returns
     -------
     k_QN : 2D numpy array, shape (Q, N)
@@ -66,7 +37,12 @@ def calc_sqexp_kernel(x_QF, x_train_NF=None, length_scale=1.0):
     N, F2 = x_train_NF.shape
     assert F == F2
 
+    assert length_scale > 0
+    
     k_QN = np.zeros((Q, N))
+    for i in range(Q):
+        for  j in range(N):
+            k_QN[i][j] = np.exp(-(np.sum(np.square(x_train_NF[j] - x_QF[i])) / np.square(length_scale)))
     # TODO compute kernel between rows of x_QF and rows of x_train_NF
 
     # Ensure the kernel matrix positive definite
@@ -74,3 +50,34 @@ def calc_sqexp_kernel(x_QF, x_train_NF=None, length_scale=1.0):
     M = np.minimum(Q, N)
     k_QN[:M, :M] += 1e-08 * np.eye(M)
     return k_QN
+
+#
+# np.set_printoptions(precision=3, suppress=1)
+#
+# # Example 1: Simple kernel evaluations with F=1 features
+# x_zero_11 = np.asarray([[0.0]])
+# x_one_11 = np.asarray([[1.0]])
+# k_11 = calc_sqexp_kernel(x_zero_11, x_zero_11, length_scale=1.0)
+# print(k_11.ndim)
+# # 2
+# print(k_11)
+# # array([[1.]])
+# print(calc_sqexp_kernel(x_one_11, x_one_11, length_scale=1.0))
+# # array([[1.]])
+# print(calc_sqexp_kernel(x_one_11, x_zero_11, length_scale=1.0))
+# # array([[0.368]])
+#
+#
+# # Example 2: Kernel evaluations with F=2 features and several examples at once
+# x_train_32 = np.asarray([[0.0, 0.0], [1.0, 1.0], [2.0, 2.0]])
+# k_33 = calc_sqexp_kernel(x_train_32, x_train_32)
+# print(k_33)
+# # array([[1.   , 0.135, 0.   ],
+# #        [0.135, 1.   , 0.135],
+# #        [0.   , 0.135, 1.   ]])
+#
+# x_test_22 = np.asarray([[-1.0, -2.0], [2.2, 0.8]])
+# k_23 = calc_sqexp_kernel(x_test_22, x_train_32)
+# print(k_23)
+# # array([[0.007, 0.   , 0.   ],
+# #        [0.004, 0.228, 0.228]])
